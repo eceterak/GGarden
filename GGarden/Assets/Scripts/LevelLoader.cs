616f7aad8905680804cb94a22b97cbe54cf5b016
Marek Bartula <bartula.marek@gmail.com>
2019-12-11 19:39:28 +0100

    Revert "Revert "Animations""
    This reverts commit 18c416df2e7603176f230ea7f4fec218670e64f3.

---
+++ GGarden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 3;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScene());
+        }
+
+    }
+
+    IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(timeToWait);
+
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
